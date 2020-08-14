5a6e0f853dda6b9128742504048f4bc83907da6d
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-08-14 15:12:39 +0100

    Update JavaProject.java

--- src/JavaProject.java
+++ src/JavaProject.java
@@ -1,7 +1,7 @@
 public class JavaProject {
     public static void main(String[] args) {
         //variables
-        int numberOfFollowers =70;
+        int numberOfFollowers = 70;
 //        numberOfFollowers=69;
         System.out.println(numberOfFollowers);
     }
